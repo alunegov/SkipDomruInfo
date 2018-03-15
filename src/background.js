c98b6d7106851b893937939d1787cf09ce956572
Alexander Lunegov <alunegov@gmail.com>
2018-03-15 21:04:40 +0500

    refactor: вывод исходного url в лог

--- src/background.js
+++ src/background.js
@@ -1,19 +1,18 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
+        console.log(`details.url = ${details.url}`)
         const url = new URL(details.url)
         const params = new URLSearchParams(url.search)
         if (params.has("ourl")) {
             const ourl = params.get("ourl")
             console.log(`redirecting to ${ourl}`)
-            return {redirectUrl: ourl}
+            return { redirectUrl: ourl }
         }
         else {
-            console.log(`canceling ${details.url}`)
-            return {cancel: true}
+            console.log(`ourl not found`)
+            return { cancel: true }
         }
     },
-    {
-        urls: ["*://info.ertelecom.ru/*"]
-    },
+    { urls: ["*://info.ertelecom.ru/*"] },
     ["blocking"]
 );
