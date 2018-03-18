214f88caf11a0710ed4cf364285dbdbeccaa33c7
Alexander Lunegov <alunegov@gmail.com>
2018-03-18 17:05:48 +0500

    refactor: add eslint, fix eslint recommendations

--- src/background.js
+++ src/background.js
@@ -1,18 +1,19 @@
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        console.log(`details.url = ${details.url}`)
-        const url = new URL(details.url)
-        const params = new URLSearchParams(url.search)
-        if (params.has("ourl")) {
-            const ourl = params.get("ourl")
-            console.log(`redirecting to ${ourl}`)
-            return { redirectUrl: ourl }
-        }
-        else {
-            console.log(`ourl not found`)
-            return { cancel: false }
-        }
-    },
-    { urls: ["*://info.ertelecom.ru/*"] },
-    ["blocking"]
+  (details) => {
+    console.log(`details.url = ${details.url}`);
+    const url = new URL(details.url);
+    const params = new URLSearchParams(url.search);
+    let resp;
+    if (params.has('ourl')) {
+      const ourl = params.get('ourl');
+      console.log(`redirecting to ${ourl}`);
+      resp = { redirectUrl: ourl };
+    } else {
+      console.log('ourl not found');
+      resp = { cancel: false };
+    }
+    return resp;
+  },
+  { urls: ['*://info.ertelecom.ru/*'] },
+  ['blocking'],
 );
